b06d59b1e6ec33b8c7082b526d973f760f3ad23e
Voktor Vovk <vivat.bbb@gmail.com>
2025-04-01 16:33:42 +0300

    - added build in container - fix some problem with gulp subpackages

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,77 @@
-let gulp =          require('gulp');
-let sass =          require('gulp-sass');
-let browserSync =   require('browser-sync');
-let del =           require('del');
+const gulp = require('gulp');
+const sass = require('gulp-dart-sass'); // Заменяем gulp-sass на gulp-dart-sass
+const browserSync = require('browser-sync').create();
+const del = require('del');
 
-gulp.task('browserSync', function(){
-    browserSync({
+// Задача для запуска BrowserSync
+function browserSyncTask() {
+    browserSync.init({
         server: {
-            baseDir: 'app'
+            baseDir: 'app', // Указываем корневую папку для сервера
         },
-        notify: false
-    })
-});
+        notify: false, // Отключаем всплывающие уведомления
+    });
+}
 
-gulp.task('sass', function(){
-    del.sync('app/css/sass-style.css');
-    return gulp.src('app/sass/**/*.sass')
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({stream: true}));
-});
+// Задача для компиляции SASS
+function sassTask() {
+    return gulp
+      .src('app/sass/**/*.sass') // Путь к исходным SASS-файлам
+      .pipe(sass().on('error', sass.logError)) // Обрабатываем ошибки
+      .pipe(gulp.dest('app/css')) // Куда сохранять скомпилированный CSS
+      .pipe(browserSync.stream()); // Перезагружаем браузер
+}
 
-gulp.task('watch', ['browserSync', 'sass'], function(){
-    gulp.watch('app/sass/**/*.sass', ['sass']);
-    gulp.watch('app/**/*.html', browserSync.reload);
-    gulp.watch('app/css/**/*.css', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
-});
+// Задача для очистки
+function cleanTask() {
+    return del('dist/**/*'); // Удаляем все файлы в папке dist
+}
 
-gulp.task('default', ['watch'], function(){});
+// Задача наблюдения за файлами
+function watchTask() {
+    gulp.watch('app/sass/**/*.sass', sassTask); // Следим за SASS-файлами
+    gulp.watch('app/**/*.html').on('change', browserSync.reload); // Следим за HTML
+    gulp.watch('app/css/**/*.css').on('change', browserSync.reload); // Следим за CSS
+    gulp.watch('app/js/**/*.js').on('change', browserSync.reload); // Следим за JS
+}
 
-gulp.task('clean', function(){
-   return del.sync('dist/**/*');
-});
+// Задача сборки проекта
+function buildTask() {
+    // Копируем CSS
+    gulp.src('app/css/**/*')
+      .pipe(gulp.dest('dist/css'));
 
-gulp.task('build', ['clean', 'sass'], function(){
-    let buildCss = gulp.src([
-        'app/css/**/*'
-    ])
-        .pipe(gulp.dest('dist/css'));
+    // Копируем lib
+    gulp.src('app/lib/**/*')
+      .pipe(gulp.dest('dist/lib'));
 
-    let buildlib = gulp.src('app/lib/**/*')
-        .pipe(gulp.dest('dist/lib'));
+    // Копируем properties
+    gulp.src('app/properties/**/*')
+      .pipe(gulp.dest('dist/properties'));
 
-    let buildProperties = gulp.src('app/properties/**/*')
-        .pipe(gulp.dest('dist/properties'));
+    // Копируем изображения
+    gulp.src('app/img/**/*')
+      .pipe(gulp.dest('dist/img'));
 
-    let buildImg = gulp.src('app/img/**/*')
-        .pipe(gulp.dest('dist/img'));
+    // Копируем шрифты
+    gulp.src('app/fonts/**/*')
+      .pipe(gulp.dest('dist/fonts'));
 
-      let buildfonts = gulp.src('app/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts'));
+    // Копируем JS
+    gulp.src('app/js/**/*')
+      .pipe(gulp.dest('dist/js'));
 
-    let buildJs = gulp.src('app/js/**/*')
-        .pipe(gulp.dest('dist/js'));
+    // Копируем HTML
+    gulp.src('app/**/*.html')
+      .pipe(gulp.dest('dist'));
+}
 
-    let buildHtml = gulp.src('app/**/*.html')
-        .pipe(gulp.dest('dist'));
+// Определяем задачи
+exports.browserSync = browserSyncTask;
+exports.sass = sassTask;
+exports.clean = cleanTask;
+exports.watch = gulp.series(sassTask, gulp.parallel(browserSyncTask, watchTask)); // Сначала компиляция, потом наблюдение
+exports.default = exports.watch;
 
-});
+// Задача build выполняет очистку, компиляцию и копирование
+exports.build = gulp.series(cleanTask, sassTask, buildTask);
